d291b5fe6c0d8e04816e621dc217b71edeac7652
developer006 <developer.ludovic@gmail.com>
2023-06-09 02:59:09 +0300

    add welcome title when loged in

--- app/Http/Controllers/TeamsController.php
+++ app/Http/Controllers/TeamsController.php
@@ -37,7 +37,7 @@ class TeamsController extends Controller
     {
         $this->authorize('create', Teams::class);
 
-        $allCoaches = Coaches::pluck('id', 'id');
+        $allCoaches = Coaches::join('users', 'users.id', '=', 'coaches.user_id')->pluck('users.name', 'coaches.id');
 
         return view('app.all_teams.create', compact('allCoaches'));
     }
