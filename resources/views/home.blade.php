5b49275c7ef00239527e34d799a9dd3c93890a77
Ludovick Konyo <developer.ludovic@gmail.com>
2024-05-22 16:39:11 +0300

    add teams

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -39,7 +39,7 @@
             <div class="col-md-3 mb-3">
                 <div class="card bg-success text-white h-100">
                     <div class="card-body py-5">Coaches {{ $coaches->count() ?? '-' }}</div>
-                    <a href="{{ route('all-coaches.index') }}}" class="card-footer d-flex link">
+                    <a href="{{ route('all-coaches.index') }}" class="card-footer d-flex link">
                         View Details
                         <span class="ms-auto">
                             <i class="bi bi-chevron-right"></i>
@@ -59,30 +59,7 @@
                 </div>
             </div>
         </div>
-        {{-- <div class="row">
-          <div class="col-md-6 mb-3">
-            <div class="card h-100">
-              <div class="card-header">
-                <span class="me-2"><i class="bi bi-bar-chart-fill"></i></span>
-                Area Chart Example
-              </div>
-              <div class="card-body">
-                <canvas class="chart" width="400" height="200"></canvas>
-              </div>
-            </div>
-          </div>
-          <div class="col-md-6 mb-3">
-            <div class="card h-100">
-              <div class="card-header">
-                <span class="me-2"><i class="bi bi-bar-chart-fill"></i></span>
-                Area Chart Example
-              </div>
-              <div class="card-body">
-                <canvas class="chart" width="400" height="200"></canvas>
-              </div>
-            </div>
-          </div>
-        </div> --}}
+        
         <div class="row">
             <div class="col-md-12 mb-3">
                 <div class="card">
